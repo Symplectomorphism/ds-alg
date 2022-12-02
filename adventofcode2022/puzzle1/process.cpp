c4415fdbfccaa4f50ee21e296b9d26763ccdf58f
Aykut C. Satici <acsatici@gmail.com>
2022-12-02 11:30:11 -0700

    Day 1 is completed

--- adventofcode2022/puzzle1/process.cpp
+++ adventofcode2022/puzzle1/process.cpp
@@ -62,5 +62,15 @@ int main() {
     auto it = max_element(sums.begin(), sums.end());
     cout << "Max index " << it-sums.begin() << ", max value: " << *it << endl;
 
+    std::vector<int> dest(3); //largest n numbers; VLA or std::dynarray in C++14
+    std::partial_sort_copy(
+        std::begin(sums), std::end(sums), //.begin/.end in C++98/C++03
+        std::begin(dest), std::end(dest), 
+        std::greater<int>() //remove "int" in C++14
+    );
+
+    cout << dest[0] << " " << dest[1] << " " << dest[2] << " " 
+         << accumulate(dest.begin(), dest.end(), 0) <<  endl;
+
     return 0;
 }
