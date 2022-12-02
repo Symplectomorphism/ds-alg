39af3326f664b5ae970e5437c46a39649a463271
Aykut C. Satici <acsatici@gmail.com>
2022-12-01 22:21:36 -0700

    started advent of code

---
+++ .ccls-cache/@home@aykut@repos@ds-alg/adventofcode2022@puzzle1@process.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <bits/stdc++.h>
+
+class Elf {
+    public:
+        void add_cal(int c) { cal.push_back(c); }
+        vector<int> get_cal() { return cal; }
+    private:
+        vector<int> cal;
+}
+
+int main() {
+    return 0;
+}
