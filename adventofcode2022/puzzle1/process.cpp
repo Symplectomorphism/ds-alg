3fdfa13288c9696c2e38c9411d66a455fd251eb7
Aykut C. Satici <acsatici@gmail.com>
2022-12-02 11:21:33 -0700

    puzzle1 solved!

--- adventofcode2022/puzzle1/process.cpp
+++ adventofcode2022/puzzle1/process.cpp
@@ -9,10 +9,13 @@ using namespace std;
 
 class Elf {
     public:
-        void add_cal(int c) { cal.push_back(c); }
+        Elf () : cal_sum(0) {};
+        void add_cal(int c) { cal.push_back(c); cal_sum += c; }
         vector<int> get_cal() { return cal; }
+        int get_sum() { return cal_sum; }
     private:
         vector<int> cal;
+        int cal_sum;
 
         friend istream& operator >>(istream& input, Elf& e);
 };
@@ -52,8 +55,12 @@ int main() {
         elves.push_back(e);
     }
 
-    // for ( auto c : elves[251].get_cal() )
-    //     cout << c << endl;
+    vector<int> sums {};
+    for ( Elf e : elves ) 
+        sums.push_back(e.get_sum());
+
+    auto it = max_element(sums.begin(), sums.end());
+    cout << "Max index " << it-sums.begin() << ", max value: " << *it << endl;
 
     return 0;
 }
