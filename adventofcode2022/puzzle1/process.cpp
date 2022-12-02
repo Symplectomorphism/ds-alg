dfc5d41109305ecb6f3d230636a7ffa6ba27bfba
Aykut C. Satici <acsatici@gmail.com>
2022-12-02 10:13:47 -0700

    reading one set of calories

--- adventofcode2022/puzzle1/process.cpp
+++ adventofcode2022/puzzle1/process.cpp
@@ -13,14 +13,39 @@ class Elf {
         vector<int> get_cal() { return cal; }
     private:
         vector<int> cal;
+
+        friend istream& operator >>(istream& input, Elf& e);
 };
+
+istream& operator >>( istream& input, Elf& e ) {
+    string s;
+    while ( getline(input, s) )
+    {
+        if (s.empty())
+            break;
+        else {
+            istringstream tmp {s};
+            int n;
+            tmp >> n;
+            e.add_cal(n);
+        }
+    }
+    
+    return input;
+}
 
 int main() {
     vector<Elf> elves {};
-    ifstream ist {"input"};
+    ifstream ist {"myinput"};
+
+    Elf e {};
+    ist >> e;
     
-    for ( Elf e; ist >> e; ) // need to implement operator>> for Elf
-        elves.push_back(e);
+    vector<int> cals = e.get_cal();
+    for ( auto c : cals )
+        cout << c << endl;
+    // for ( Elf e; ist >> e; ) // need to implement operator>> for Elf
+    //     elves.push_back(e);
 
     return 0;
 }
