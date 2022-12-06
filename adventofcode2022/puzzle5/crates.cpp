776f5c3a73433fa8357382a1ce0cd584589a0797
Aykut C. Satici <acsatici@gmail.com>
2022-12-06 15:25:36 -0500

    start puzzle5

---
+++ adventofcode2022/puzzle5/crates.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Crate {
+
+};
+
+
+int main() {
+    return 0;
+}
